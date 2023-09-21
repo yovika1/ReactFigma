8d01ddc9816ff96ab594b1446e7644676630abaf
Yovika <yovikaranga@gmail.com>
2023-09-21 15:34:01 +0530

    ghjkl

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import TestimonalsCard from './components/Cards/TestimonalsCard';
-import Index  from './components/navbar/Index';
-import Head from './components/Header/Head';
-import Aboutme from './components/Aboutme/About';
-import Ptfolio from './components/Myprotfolio/Prtfolio';
-import Frm from './components/Form_cmpnt/Form';
+import TestimonalsCard from "./components/Cards/TestimonalsCard";
+import Index from "./components/navbar/Index";
+import Head from "./components/Header/Head";
+import Aboutme from "./components/Aboutme/About";
+import Ptfolio from "./components/Myprotfolio/Prtfolio";
+import Frm from "./components/Form_cmpnt/Form";
 
-import './index.css'
-import Skillcard from './components/Cards1/Skillcard';
+import "./index.css";
+import Skillcard from "./components/Cards1/Skillcard";
 function App() {
   // const name = "My Name is ............";
-  
+
   // const Ptfolio=[{name : "Yovi"},{name : "mande"},{name : "fghjop["}]
   return (
     <>
       <Index />
-      <Head/>
-      <div className='card1'>
-      <Skillcard/>
-      fftgyuhf;'shouldComponentUpdate(nextProps, nextState) 
-      sdgsdg
-      sdgsdgsg 
+      <Head />
+      <div className="card1">
+        <Skillcard />
       </div>
-      <Aboutme/>
-    <Ptfolio/>
+      <Aboutme />
+      <Ptfolio />
 
-      <index1/>  
-      <TestimonalsCard/>
-     <Frm/>
+      <index1 />
+      <TestimonalsCard />
+      <Frm />
     </>
-    
   );
 }
 
--- src/components/Form_cmpnt/Form.jsx
+++ src/components/Form_cmpnt/Form.jsx
@@ -1,44 +1,54 @@
-import React from 'react'
-import './frm.css'
+import React from "react";
+import "./frm.css";
 function form() {
   return (
     <>
-    <div className='get'>Get in Touch</div>
-    <div className='headcnct'>Contact Me</div>
-    <div className='descpt'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus officia veritatis harum?</div>
-    <div >
-    <form className='form'>
-      
-      <div className='name1'>
-    <label for="name"className='nme'>Name</label>
-    <input type="txt" placeholder=""></input> 
-   
-    </div>
-    <div>
-    <label for="lastname" className='lstname'>Last Name</label>
-    <input type="text" className='spc' placeholder=""></input>
-    </div>
-<div>
-      <label htmlFor="" className='Em'>Email</label>
-    <input type="txt" placeholder =''className='spc'></input>
-    </div>
-    <div>
-      <label htmlFor="" className='Phno'>Phone Number</label>
-      <input type="text"  className='spc' placeholder='' />
-    </div>
-    <div>
-      <label htmlFor="" className='slct'>Select one</label>
-      <select name="" id=""></select>
-    </div>
-    <div>
-      <label htmlFor="" className='msg'>Message</label>
-      <textarea name="" id="" cols="30" rows="10"></textarea>
-    </div>
-
-    </form>
-    </div>
-   </>
-  )
+      <div className="container">
+        <div className="get">Get in Touch</div>
+        <div className="headcnct">Contact Me</div>
+        <div className="descpt">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Necessitatibus officia veritatis harum?
+        </div>
+        <div>
+          <div className="row">
+            <div className="inp">
+              <label>First Name</label>
+              <input />
+            </div>
+            <div className="inp">
+              <label>Last Name</label>
+              <input />
+            </div>
+          </div>
+          <div className="row">
+            <div className="inp">
+              <label>Email </label>
+              <input />
+            </div>
+            <div className="inp">
+              <label>Phone Number</label>
+              <input />
+            </div>
+          </div>
+          <div className="inp">
+            <label htmlFor="">Chosse Topic</label>
+            <select name="" id="">
+              <option value="">ghjkl;'</option>
+              <option value="">ghjkl;'</option>
+              <option value="">ghjkl;'</option>
+              <option value="">ghjkl;'</option>
+            </select>
+          </div>
+          <div className="inp">
+          <label htmlFor="">Message</label>
+          <textarea rows={10} />
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default form;
